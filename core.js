c0766248bd487599f48fb0da13667021727012b9
Gam000!$ <gam002@qq.com>
2022-08-27 23:08:05 +0800

    update

--- core.js
+++ core.js
@@ -1,10 +1,7 @@
-debugger;
 import workerTimer from './WorkTimer.js';
-debugger;
 const currentInterval = workerTimer.setInterval(() => {
     console.log('===================================================');
 }, 3000);
-debugger;
 workerTimer.setTimeout(() => {
     console.warn('-----------------------------------------> 撤销Interval ', currentInterval);
     workerTimer.clearInterval(currentInterval);
