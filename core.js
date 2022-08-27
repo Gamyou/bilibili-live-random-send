d282eacd2442777ea99494751733d6a8b0c99264
Gam000!$ <gam002@qq.com>
2022-08-27 23:28:44 +0800

    删除core.js文件

--- core.js
+++
@@ -1,8 +0,0 @@
-import workerTimer from './WorkTimer.js';
-const currentInterval = workerTimer.setInterval(() => {
-    console.log('===================================================');
-}, 3000);
-workerTimer.setTimeout(() => {
-    console.warn('-----------------------------------------> 撤销Interval ', currentInterval);
-    workerTimer.clearInterval(currentInterval);
-}, 30000);
