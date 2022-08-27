fd4f5a271698d258485dfacd9d9364a0d59776da
Gam000!$ <gam002@qq.com>
2022-08-27 20:34:26 +0800

    add core.js and prod dir.

---
+++ core.js
@@ -0,0 +1,6 @@
+import workerTimer from 'WorkTimer.js';
+debugger;
+const currentInterval = workerTimer.setInterval(() => {
+    console.log('===================================================');
+}, 3000);
+debugger;
